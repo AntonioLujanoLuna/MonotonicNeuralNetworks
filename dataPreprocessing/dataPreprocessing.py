567aeca8192bfc1bf7c2a0505f3c98f718d578cc
antoniolujano_dataSpell <a00lujano@gmail.com>
2024-07-16 20:22:06 +0200

    Min max architectures

--- dataPreprocessing/dataPreprocessing.py
+++ dataPreprocessing/dataPreprocessing.py
@@ -1,169 +1,169 @@
 
 # Monotoniciy of the features in the datasets
 
-    """
-    Abalone dataset monotonicity
-        { 'Sex_0': 0,
-            'Sex_1': 0,
-            'Sex_2': 0,
-            'Length': 0,
-            'Diameter': 0,
-            'Height': 0,
-            'Whole weight': 1,
-            'Shucked weight': 1,
-            'Viscera weight': 1,
-            'Shell weight': 1}
-        }
-
-    Auto-mpg dataset monotonicity
-    {   "cylinders": -1,
-        "displacement": -1,
-        "horsepower": -1,
-        "weight": -1,
-        "acceleration": 1,
-        "model_Year": 1,
-        "origin": 1,
+"""
+Abalone dataset monotonicity
+    { 'Sex_0': 0,
+        'Sex_1': 0,
+        'Sex_2': 0,
+        'Length': 0,
+        'Diameter': 0,
+        'Height': 0,
+        'Whole weight': 1,
+        'Shucked weight': 1,
+        'Viscera weight': 1,
+        'Shell weight': 1}
     }
 
-    BlogFeedback dataset monotonicity
+Auto-mpg dataset monotonicity
+{   "cylinders": -1,
+    "displacement": -1,
+    "horsepower": -1,
+    "weight": -1,
+    "acceleration": 1,
+    "model_Year": 1,
+    "origin": 1,
+}
 
-    {
-    ...
-    col51: 1,
-    col52: 1,
-    col53: 1,
-    col54: 1,
-    col55: 1,
-    col56: 1,
-    col57: 1,
-    col58: 1,
-    col59: 1,
-    ...
-    }
+BlogFeedback dataset monotonicity
 
-    Boston Housing dataset monotonicity
-    {
-        'CRIM': -1,
-         'ZN': 0,
-         'INDUS': 0,
-         'CHAS': 0,
-         'NOX': 0,
-         'RM': 1,
-         'AGE': 0,
-         'DIS': 0,
-         'RAD': 0,
-         'TAX': 0,
-         'PTRATIO': 0,
-         'B': 0,
-         'LSTAT': 0
-     }
+{
+...
+col51: 1,
+col52: 1,
+col53: 1,
+col54: 1,
+col55: 1,
+col56: 1,
+col57: 1,
+col58: 1,
+col59: 1,
+...
+}
 
-    Compas dataset monotonicity
+Boston Housing dataset monotonicity
+{
+    'CRIM': -1,
+     'ZN': 0,
+     'INDUS': 0,
+     'CHAS': 0,
+     'NOX': 0,
+     'RM': 1,
+     'AGE': 0,
+     'DIS': 0,
+     'RAD': 0,
+     'TAX': 0,
+     'PTRATIO': 0,
+     'B': 0,
+     'LSTAT': 0
+ }
 
-    {
-        'prior_count': 1,
-        'juv_fel_count': 1,
-        'juv_misd_count': 1,
-        'juv_other_count': 1,
-        'age': 0,
-        'race_0': 0,
-        'race_1': 0,
-        'race_2': 0,
-        'race_3': 0,
-        'race_4': 0,
-        'race_5': 0,
-        'sex_0': 0,
-        'sex_1': 0
-    }
+Compas dataset monotonicity
 
-    Era dataset monotonicity
-    {
-        'in1': 1,
-        'in2': 1,
-        'in3': 1,
-        'in4': 1,
-    }
+{
+    'prior_count': 1,
+    'juv_fel_count': 1,
+    'juv_misd_count': 1,
+    'juv_other_count': 1,
+    'age': 0,
+    'race_0': 0,
+    'race_1': 0,
+    'race_2': 0,
+    'race_3': 0,
+    'race_4': 0,
+    'race_5': 0,
+    'sex_0': 0,
+    'sex_1': 0
+}
 
-    ESL dataset monotonicity
+Era dataset monotonicity
+{
+    'in1': 1,
+    'in2': 1,
+    'in3': 1,
+    'in4': 1,
+}
 
-    {
-        'in1': 1,
-        'in2': 1,
-        'in3': 1,
-        'in4': 1,
-    }
+ESL dataset monotonicity
 
-    Heart dataset monotonicity
+{
+    'in1': 1,
+    'in2': 1,
+    'in3': 1,
+    'in4': 1,
+}
 
-    {
-        "age": 0,
-        "sex": 0,
-        "cp": 0,
-        "trestbps": 1,
-        "chol": 1,
-        "fbs": 0,
-        "restecg": 0,
-        "thalach": 0,
-        "exang": 0,
-        "oldpeak": 0,
-        "slope": 0,
-        "ca": 0,
-        "thal": 0,
-    }
+Heart dataset monotonicity
 
-    Lev dataset monotonicity
+{
+    "age": 0,
+    "sex": 0,
+    "cp": 0,
+    "trestbps": 1,
+    "chol": 1,
+    "fbs": 0,
+    "restecg": 0,
+    "thalach": 0,
+    "exang": 0,
+    "oldpeak": 0,
+    "slope": 0,
+    "ca": 0,
+    "thal": 0,
+}
 
-    {
-        'In1': 1,
-        'In2': 1,
-        'In3': 1,
-        'In4': 1,
-    }
+Lev dataset monotonicity
 
-    Loan dataset monotonicity
-        {'feature_0': -1,
-         'feature_1': 1,
-         'feature_2': -1,
-         'feature_3': -1,
-         'feature_4': 1,
-         'feature_5': 0,
-         'feature_6': 0,
-         'feature_7': 0,
-         'feature_8': 0,
-         'feature_9': 0,
-         'feature_10': 0,
-         'feature_11': 0,
-         'feature_12': 0,
-         'feature_13': 0,
-         'feature_14': 0,
-         'feature_15': 0,
-         'feature_16': 0,
-         'feature_17': 0,
-         'feature_18': 0,
-         'feature_19': 0,
-         'feature_20': 0,
-         'feature_21': 0,
-         'feature_22': 0,
-         'feature_23': 0,
-         'feature_24': 0,
-         'feature_25': 0,
-         'feature_26': 0,
-         'feature_27': 0
-    }
+{
+    'In1': 1,
+    'In2': 1,
+    'In3': 1,
+    'In4': 1,
+}
 
-    Swd dataset monotonicity
+Loan dataset monotonicity
+    {'feature_0': -1,
+     'feature_1': 1,
+     'feature_2': -1,
+     'feature_3': -1,
+     'feature_4': 1,
+     'feature_5': 0,
+     'feature_6': 0,
+     'feature_7': 0,
+     'feature_8': 0,
+     'feature_9': 0,
+     'feature_10': 0,
+     'feature_11': 0,
+     'feature_12': 0,
+     'feature_13': 0,
+     'feature_14': 0,
+     'feature_15': 0,
+     'feature_16': 0,
+     'feature_17': 0,
+     'feature_18': 0,
+     'feature_19': 0,
+     'feature_20': 0,
+     'feature_21': 0,
+     'feature_22': 0,
+     'feature_23': 0,
+     'feature_24': 0,
+     'feature_25': 0,
+     'feature_26': 0,
+     'feature_27': 0
+}
 
-    {
-        'In1': 1,
-        'In2': 1,
-        'In3': 1,
-        'In4': 0,
-        'In5': 1,
-        'In6': 0,
-        'In7': 1,
-        'In8': 0,
-        'In9': 1,
-        'In10': 1,
-    }
+Swd dataset monotonicity
 
-    """
+{
+    'In1': 1,
+    'In2': 1,
+    'In3': 1,
+    'In4': 0,
+    'In5': 1,
+    'In6': 0,
+    'In7': 1,
+    'In8': 0,
+    'In9': 1,
+    'In10': 1,
+}
+
+"""
