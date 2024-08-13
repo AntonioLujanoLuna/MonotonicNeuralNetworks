15ee4f38c5edf844759f5e5684f76c19ebad62fb
AntonioLujanoLuna <a00lujano@gmail.com>
2024-08-13 23:37:38 +0200

    prepping mono losses exps

--- src/run.sh
+++ src/run.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Activate the virtual environment
+source /home/antoniolujano/.virtualenvs/MonotonicNeuralNetworks/bin/activate
+
+# Add the parent directory to PYTHONPATH
+export PYTHONPATH="${PYTHONPATH}:$(pwd)/.."
+
 # Script to run exps
 
-python_files = (
-"exp/3_expsMinMax.py"
-"exp/4_expsHLL.py"
+python_files=(
 "exp/9_expsPWL.py"
 "exp/10_expsCMNN.py"
 "exp/11_expsPWLMixup.py"
+"exp/3_expsMinMax.py"
+"exp/4_expsHLL.py"
 )
 
 for file in "${python_files[@]}"; do
@@ -20,3 +26,6 @@ for file in "${python_files[@]}"; do
 done
 
 echo "All executed"
+
+# Deactivate the virtual environment when done
+deactivate
